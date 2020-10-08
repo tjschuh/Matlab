4b5eafc7e786bd60fd258fbd57f5c434bfdc3e99
Terance Schuh <tschuh@princeton.edu>
2020-10-08 01:28:37 -0400

    current version, is a function now

--- chplot.m
+++ chplot.m
@@ -1,4 +1,9 @@
-for file = 0
+function chplot(numfiles)
+
+
+
+
+for file = 697:numfiles
 file
 % Open file, turn it into matrix, and then
 % reshape it into a 4 "channel" matrix
@@ -7,18 +12,22 @@ fid = fopen(str);
 raw = fread(fid,inf,'int16');
 FourChan = reshape(raw,4,[]);
 
-run challocate.m
+[shift,jumps] = challocate(FourChan);
 
 % Plotting section
+% adjust min/max thing!
 for i = 1:4
+avg = mean(shift(i,:));
     if i == 1
       subplot(4,1,1);
       plot(shift(1,:),'color',[0.4660 0.6740 0.1880])
+      yline(avg)
       title('Pre-Amped Acoustic Data')
       xlim([0 24000000])
       xticks(0:2000000:24000000)
       xticklabels({})
-      %ylim([-250 250])
+      ylim([min(shift(1,:))-abs(.05*min(shift(1,:))) max(shift(1,:))+(.05*max(shift(1,:)))])
+      yticks([min(shift(1,:)) round(avg) max(shift(1,:))])
       ax = gca;
       ax.XGrid = 'on';
       ax.YGrid = 'off';
@@ -27,11 +36,13 @@ for i = 1:4
     elseif i == 2
       subplot(4,1,2);
       plot(shift(2,:),'color',[0.6350 0.0780 0.1840])
+      yline(avg)
       title('Bandpassed Acoustic Data')
       xlim([0 24000000])
       xticks(0:2000000:24000000)
       xticklabels({})
-      %ylim([7000 7600])
+      ylim([min(shift(2,:))-abs(.01*min(shift(2,:))) max(shift(2,:))+(.01*max(shift(2,:)))])
+      yticks([min(shift(2,:)) round(avg) max(shift(2,:))])
       ax = gca;
       ax.XGrid = 'on';
       ax.YGrid = 'off';
@@ -40,6 +51,7 @@ for i = 1:4
     elseif i == 3
       subplot(4,1,3);
       plot(shift(3,:),'k')
+      %yline(avg)
       title('Time')
       xlim([0 24000000])
       xticks(0:2000000:24000000)
@@ -54,12 +66,14 @@ for i = 1:4
     else
       subplot(4,1,4);
       plot(shift(4,:),'color',[0 0.4470 0.7410])
+      yline(avg)
       title('Low-Frequency Hydrophone')
       xlim([0 24000000])
       xticks(0:2000000:24000000)
       xticklabels({'0','5','10','15','20','25','30','35','40','45','50','55','60'})
       xlabel('Time (seconds)')
-      %ylim([3400 3550])
+      ylim([min(shift(4,:))-abs(.01*min(shift(4,:))) max(shift(4,:))+(.01*max(shift(4,:)))])
+      yticks([min(shift(4,:)) round(avg) max(shift(4,:))])
       ax = gca;
       ax.XGrid = 'on';
       ax.YGrid = 'off';
@@ -68,6 +82,8 @@ for i = 1:4
     end
 end
 sgtitle(['File ',num2str(file)])
+a = annotation('textbox',[0.77 0.94 0 0],'String',['# of jumps = ' num2str(jumps)],'FitBoxToText','on');
+a.FontSize = 12;
 
 % this section adds leading zeros to the pdf names	 
 if numel(num2str(file)) == 1
@@ -84,6 +100,8 @@ saveas(gcf,filename);
 
 fclose(fid);
 
-%clearvars -except file
+clf
 
 end
+
+%totnumfiles = numfiles + 1; %accounts for file 0
