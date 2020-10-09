4dfc8c8f6ffc3c49c6b08a0c845e97320c31591a
Frederik J. Simons <fjsimons@gmail.com>
2020-10-09 16:59:09 -0400

    A channel plotter

--- chplot.m
+++ chplot.m
@@ -14,6 +14,8 @@ function chplot(firstfile,lastfile)
 
 % Assume the record length in seconds
 rlens=60;
+% Define a random segment length for inspection in seconds
+rseg=10;
 % Assume the sampling rate
 Fs=400000;
 % Moving average in seconds
@@ -32,14 +34,13 @@ for file = firstfile:lastfile
   % Allocate the channels and identify jumps
   [FourChan,jumps] = challocate(FourChan);
 
-  %4000000 = 10 seconds
-  lowbound = randi(size(FourChan,2)-Fs-1);
-  upbound = lowbound + Fs-1;
+  % Define a random segment of a certain length
+  lowbound = randi(size(FourChan,2)-rseg*Fs-1);
+  upbound = lowbound + rseg*Fs-1;
   sub = FourChan(:,lowbound:upbound);
-  onesec = 400000;
-  bot = onesec*ceil(lowbound/onesec);
-  top = onesec*floor(upbound/onesec);
-  tensec = [bot:onesec:top];
+  bot = Fs*ceil(lowbound/Fs);
+  top = Fs*floor(upbound/Fs);
+  tensec = [bot:Fs:top];
 
   % Plotting section
   % adjust min/max thing! [left bottom width height]
@@ -47,7 +48,7 @@ for file = firstfile:lastfile
   % xtix={'0','5','10','15','20','25','30','35','40','45','50','55','60'};
   xtixl1=0:5:60;
   xtix1=xtixl1*Fs;
-  xtixl2={tensec./onesec};
+  xtixl2={tensec./Fs};
   %ylims=[1 2 ; 3 4 ; 5 6 ; 7 8];
   titl={'Pre-Amped Acoustic Data','Bandpassed Acoustic Data','Time','Low-Frequency Hydrophone'};
 
@@ -60,38 +61,32 @@ for file = firstfile:lastfile
       subplot(4,2,1);
       plot(FourChan(1,:),'color',[0.4660 0.6740 0.1880])
       %yline(avg)
-      xlim([0 rlens*Fs])
       ylim([min(FourChan(1,:))-abs(.05*min(FourChan(1,:))) max(FourChan(1,:))+(.05*max(FourChan(1,:)))])
       yticks([min(FourChan(1,:)) round(avg) max(FourChan(1,:))])
-      priti(gca,xtixl1,titl{1},xtix1)
-      subplot(4,2,2);
+      priti(gca,xtixl1,titl{1},xtix1,[0 rlens*Fs])
+      subplot(4,2,2)
       plot(sub(1,:),'color',[0.4660 0.6740 0.1880])
-      xlim([0 4000000])
-      priti(gca,xtixl2,[],(bot-lowbound):onesec:(Fs-1-(upbound-top)))
+      priti(gca,xtixl2,[],(bot-lowbound):Fs:(Fs-1-(upbound-top)),[0 rseg*Fs])
     elseif i == 2
       subplot(4,2,3);
       plot(FourChan(2,:),'color',[0.6350 0.0780 0.1840])
       %yline(avg)
-      xlim([0 rlens*Fs])
       ylim([min(FourChan(2,:))-abs(.01*min(FourChan(2,:))) max(FourChan(2,:))+(.01*max(FourChan(2,:)))])
       yticks([min(FourChan(2,:)) round(avg) max(FourChan(2,:))])
-      priti(gca,xtixl1,titl{2},xtix1)
-      subplot(4,2,4);
+      priti(gca,xtixl1,titl{2},xtix1,[0 rlens*Fs])
+      subplot(4,2,4)
       plot(sub(2,:),'color',[0.6350 0.0780 0.1840])
-      xlim([0 4000000])
-      priti(gca,xtixl2,[],(bot-lowbound):onesec:(Fs-1-(upbound-top)))
+      priti(gca,xtixl2,[],(bot-lowbound):Fs:(Fs-1-(upbound-top)),[0 rseg*Fs])
     elseif i == 3
       subplot(4,2,5);
       plot(FourChan(3,:),'k')
       %yline(avg)
-      xlim([0 rlens*Fs])
       ylim([4900 5700])
       yticks([5000 5600])
-      priti(gca,xtixl1,titl{3},xtix1)
+      priti(gca,xtixl1,titl{3},xtix1,[0 rlens*Fs])
       subplot(4,2,6);
       plot(sub(3,:),'k')
-      xlim([0 4000000])
-      priti(gca,xtixl2,[],(bot-lowbound):onesec:(Fs-1-(upbound-top)))
+      priti(gca,xtixl2,[],(bot-lowbound):Fs:(Fs-1-(upbound-top)),[0 rseg*Fs])
     else
       subplot(4,2,7);
       plot(FourChan(4,:),'color',[0 0.4470 0.7410])
@@ -102,14 +97,13 @@ for file = firstfile:lastfile
       xlabel('Time [s]')
       ylim([min(FourChan(4,:))-abs(.01*min(FourChan(4,:))) max(FourChan(4,:))+(.01*max(FourChan(4,:)))])
       yticks([min(FourChan(4,:)) round(avg) max(FourChan(4,:))])
-      priti(gca,xtixl1,titl{4},xtix1)
+      priti(gca,xtixl1,titl{4},xtix1,[0 rlens*Fs])
       subplot(4,2,8);
       plot(sub(4,:),'color',[0 0.4470 0.7410])
       hold on
       plot(mavgsub)
-      xlim([0 4000000])
       xlabel('Time [s])')
-      priti(gca,xtixl2,[],(bot-lowbound):onesec:(Fs-1-(upbound-top)))
+      priti(gca,xtixl2,[],(bot-lowbound):Fs:(Fs-1-(upbound-top)),[0 rseg*Fs])
     end
   end
   sgtitle(['Minute ',num2str(file)])
@@ -124,11 +118,12 @@ for file = firstfile:lastfile
 end
 
 % Cosmetics
-function priti(ax,xtil,tit,xtic)
+function priti(ax,xtil,tit,xtic,xlimi)
 ax.XGrid = 'on';
 ax.YGrid = 'off';
 ax.GridColor = [0 0 0];
 ax.TickLength = [0 0];
+xlim(xlimi)
 xticks(xtic)
 xticklabels(xtil)
 title(tit)
