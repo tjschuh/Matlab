53ba4c8bae90f9578ed91eea470f0a944b42b075
Terance Schuh <tschuh@princeton.edu>
2020-12-11 14:12:49 -0500

    A cross-correlation tool

--- chcross.m
+++ chcross.m
@@ -15,42 +15,9 @@ function [tsample1,cross,mc,mcloc] = chcross(FourChan,rlens,xver)
 %
 % Written by tschuh@princeton.edu, 10/30/2020
 
-%first one-second time snippet from FourChan
-%tsample1 = FourChan(3,1:400000);
-%second one-second time snipet from FourChan
-%tsample2 = FourChan(3,400001:800000);
-
-%cross-correlation of tsample1 with itself, normalized
-%autocross = xcorr(tsample1)/max(xcorr(tsample1)); 
-
-%cross = xcorr(tsample1,tsample2)/max(xcorr(tsample1,tsample2));
-  cross = NaN;
-%figure
-%plot(autocross)
-%title('Cross-Correlation')
-%hold on
-%plot(cross)
-
-%first one-second time snippet from FourChan  
-
-
-%loop that computes subsequent one-second time snippets
-%from FourChan and then cross-correlates them with tsample1
-%figure
-%title('Cross-Correlation')
-%for i = 1:rlens
-    %tsample2 = FourChan(3,1+400000*(i-1):400000*i);
-    %[c,lags] = xcorr(tsample1,tsample2);
-    %stem(lags(1:10000:end),c(1:10000:end))
-    %if i == rlens
-     % hold off
-    %else
-     % hold on
-    %end
-%end
-
 %working code
-  
+
+%zero-out the time channel of FourChan  
 FourChan(3,:) = FourChan(3,:) - min(FourChan(3,:));
 %length of 1 second segment
 sampsize = 400000;
@@ -64,33 +31,28 @@ for i = 1:rlens
   tsample2 = FourChan(3,1+sampsize*(i-1):sampsize*i);
   %cross-correlation
   [c,lags] = xcorr(tsample1,tsample2,'coeff');
-  if xver == 1
-  subplot(1,2,1)
-  %plot subsample version
-  plot(lags(1:intv:end),c(1:intv:end))
-  hold on
-  end
-end
-
-for i = 1:rlens
-  tsample1 = FourChan(3,1:sampsize);
-  tsample2 = FourChan(3,1+sampsize*(i-1):sampsize*i);
-  [c,lags] = xcorr(tsample1,tsample2,'coeff');
-  dummy(i,:) = c;
+  cross(i,:) = c;
   [mc(i),mcloc(i)] = max(c);
+  mcloc(i) = mcloc(i) - sampsize;
   if xver == 1
-    subplot(1,2,2)
+    %plot cross-correlations
+    subplot(1,2,1)
     plot(lags(1:intv:end),c(1:intv:end))
+    title('Cross-Correlations of Second 1 with all other Seconds')
+    hold on
+    %plot same cross-correlations, but zoomed in
+    subplot(1,2,2)
+    plot(lags(1:intv:end),c(1:intv:end), ...
+	 'LineWidth',1,'Color',[0 0 0]+i/(rlens+1))
+    title(sprintf('%6.4f at %4i',mc(i),mcloc(i)),'FontSize',20)
+    ylim([0.979 1.001])
     if i == rlens
-      ylim([min(dummy(:,sampsize))-0.001*min(dummy(:,sampsize)) ...
-					    max(dummy,[],'all')+0.001*max(dummy,[],'all')])
+      ylim([min(cross(:,sampsize))-0.001*min(cross(:,sampsize)) ...
+		max(cross,[],'all')+0.001*max(cross,[],'all')])
     end
     hold on
-    title(sprintf('%6.4f at %4i',mc(i),mcloc(i)-sampsize*i,'FontSize',20)
-    ylim([0.95 1.005])
-    pause
+    %pause
   end
-  mcloc(i) = mcloc(i) - sampsize*i;  
 end
 
 %keyboard
