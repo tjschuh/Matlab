3e6f578dcc0fffacd00660e8ff5743263966bcba
Terance Schuh <tschuh@princeton.edu>
2020-12-17 15:37:24 -0500

    A channel plotter

--- chplot.m
+++ chplot.m
@@ -65,6 +65,11 @@ for file = firstfile:lastfile
 
   % Plotting section, called only if plotver is "on"
   if plotver == 1
+
+  % Set default figure position, [left bottom width height]
+  set(0,'defaultfigureposition',[550 150 850 650])
+  %previously set as [500 500 600 600]
+  
   % Define a random segment of a certain length
   lowbound = randi(size(FourChan,2)-rseg*Fs-1);
   upbound = lowbound + rseg*Fs-1;
@@ -73,78 +78,122 @@ for file = firstfile:lastfile
   top = Fs*floor(upbound/Fs);
   tensec = [bot:Fs:top];
 
-  % Define plotting variables that are neccessary for "cosmo" function below
-  set(0,'defaultfigureposition',[500 500 600 600]) %[left bottom width height]
-  xtixl1 = 0:5:60;
-  xtix1 = xtixl1*Fs;
-  xlimit1 = [0 rlens*Fs];
-  xtixl2 = tensec/Fs;
-  xtix2 = (bot-lowbound):Fs:((10*Fs)-1-(upbound-top));
-  xlimit2 = [0 rseg*Fs];
-  titl = {'PPS & Timestamp', ...
-          'Pre-Amped 400 kHz Acoustic Data', ...
-          '12.5 kHz Bandpassed Acoustic Data', ...
-	  'Low-Frequency Hydrophone'};
+  % Calculate some averages
+  for i = 1:size(FourChan,1)
+    avg(i) = mean(FourChan(i,:));
+  end  
+  mavgFourChan = movmean(FourChan(4,:),maslr);
+  mavgsub = movmean(sub(4,:),maslr);
+
+  % Open a figure to not overwrite cross-correlation plot
+  figure
+
+  % Colors
+  color1 = [0 0 0];
+  color2 = [0.4660 0.6740 0.1880];
+  color3 = [0.6350 0.0780 0.1840];
+  color4 = [0 0.4470 0.7410];
+
+  % Plot PSD
+  xtixl1 = 0:25:200;
+  xtix1 = xtixl1*1000;
+  xlimit1 = [0 Fs/2];
+  titl1 = {'PSD Estimate'};
   
-  % Loop over the panels
-  figure %this is needed to not overwrite cross-correlation figure
-  for i = 1:4
-    avg = mean(FourChan(i,:));
-    mavgFourChan = movmean(FourChan(4,:),maslr);
-    mavgsub = movmean(sub(4,:),maslr);
-    if i == 1
-      %ah(i) = subplot(4,2,1);
-      subplot(4,2,1);
-      plot(FourChan(1,:),'k')
-      %yline(avg)
-      ylim([4900 5700])
-      yticks([5000 5600])
-      cosmo(gca,titl{1},xlimit1,xtix1,[])
-      subplot(4,2,2)
-      plot(sub(1,:),'k')
-      ylim([4900 5700])
-      yticks([5000 5600])
-      cosmo(gca,[],xlimit2,xtix2,[])
-    elseif i == 2
-      %ah(i) = subplot(4,2,3);
-      subplot(4,2,3);
-      plot(FourChan(2,:),'color',[0.4660 0.6740 0.1880])
-      %yline(avg)
-      ylim([min(FourChan(2,:))-abs(.05*min(FourChan(2,:))) max(FourChan(2,:))+(.05*max(FourChan(2,:)))])
-      yticks([min(FourChan(2,:)) round(avg) max(FourChan(2,:))])
-      cosmo(gca,titl{2},xlimit1,xtix1,[])
-      subplot(4,2,4)
-      plot(sub(2,:),'color',[0.4660 0.6740 0.1880])
-      cosmo(gca,[],xlimit2,xtix2,[])
-    elseif i == 3
-      subplot(4,2,5);
-      plot(FourChan(3,:),'color',[0.6350 0.0780 0.1840])
-      %yline(avg)
-      ylim([min(FourChan(3,:))-abs(.01*min(FourChan(3,:))) max(FourChan(3,:))+(.01*max(FourChan(3,:)))])
-      yticks([min(FourChan(3,:)) round(avg) max(FourChan(3,:))])
-      cosmo(gca,titl{3},xlimit1,xtix1,[])
-      subplot(4,2,6);
-      plot(sub(3,:),'color',[0.6350 0.0780 0.1840]) 
-      cosmo(gca,[],xlimit2,xtix2,[])
-    else
-      subplot(4,2,7);
-      plot(FourChan(4,:),'color',[0 0.4470 0.7410])
-      hold on
-      plot(mavgFourChan,'LineWidth',1.75)
-      %yline(avg)
-      xlabel('Time [s]')
-      ylim([min(FourChan(4,:))-abs(.01*min(FourChan(4,:))) max(FourChan(4,:))+(.01*max(FourChan(4,:)))])
-      yticks([min(FourChan(4,:)) round(avg) max(FourChan(4,:))])
-      cosmo(gca,titl{4},xlimit1,xtix1,xtixl1)
-      subplot(4,2,8);
-      plot(sub(4,:),'color',[0 0.4470 0.7410])
-      hold on
-      plot(mavgsub,'LineWidth',1.75)
-      xlabel('Time [s]')
-      cosmo(gca,[],xlimit2,xtix2,xtixl2)
-    end
-  end
+  subplot(4,3,1)
+  [pxx,f] = periodogram(FourChan(1,:),hamming(length(FourChan(1,:))), ...
+	    length(FourChan(1,:)),Fs);
+  plot(f,10*log10(pxx),'color',color1)
+  cosmo(gca,titl1{1},xlimit1,xtix1,[])
+
+  subplot(4,3,4)
+  [pxx,f] = periodogram(FourChan(2,:),hamming(length(FourChan(2,:))), ...
+	    length(FourChan(2,:)),Fs);
+  plot(f,10*log10(pxx),'color',color2)
+  cosmo(gca,[],xlimit1,xtix1,[])
+  
+  subplot(4,3,7)
+  [pxx,f] = periodogram(FourChan(3,:),hamming(length(FourChan(3,:))), ...
+	    length(FourChan(3,:)),Fs);
+  plot(f,10*log10(pxx),'color',color3)
+  cosmo(gca,[],xlimit1,xtix1,[])
+  
+  subplot(4,3,10)
+  [pxx,f] = periodogram(FourChan(4,:),hamming(length(FourChan(4,:))), ...
+	    length(FourChan(4,:)),Fs);
+  plot(f,10*log10(pxx),'color',color4)
+  cosmo(gca,[],xlimit1,xtix1,xtixl1)
+  xlabel('Frequency [kHz]')
+  ylabel({'Power/frequency','[dB/Hz]'})
+  
+  % Plot FourChan
+  xtixl2 = 0:5:60;
+  xtix2 = xtixl2*Fs;
+  xlimit2 = [0 rlens*Fs];
+  titl2 = {'PPS & Timestamp', 'Pre-Amped 400 kHz Acoustic Data', ...
+          '12.5 kHz Bandpassed Acoustic Data', 'Low-Frequency Hydrophone'};
+
+  %ah(i) = subplot(4,2,2);
+  subplot(4,3,2);
+  plot(FourChan(1,:),'color',color1)
+  ylim([4900 5700])
+  yticks([5000 5600])
+  cosmo(gca,titl2{1},xlimit2,xtix2,[])
+
+  %ah(i) = subplot(4,2,5);
+  subplot(4,3,5);
+  plot(FourChan(2,:),'color',color2)
+  ylim([min(FourChan(2,:))-abs(.05*min(FourChan(2,:))) ...
+	max(FourChan(2,:))+(.05*max(FourChan(2,:)))])
+  yticks([min(FourChan(2,:)) round(avg(2)) max(FourChan(2,:))])
+  cosmo(gca,titl2{2},xlimit2,xtix2,[])
+
+  subplot(4,3,8);
+  plot(FourChan(3,:),'color',color3)
+  ylim([min(FourChan(3,:))-abs(.01*min(FourChan(3,:))) ...
+	max(FourChan(3,:))+(.01*max(FourChan(3,:)))])
+  yticks([min(FourChan(3,:)) round(avg(3)) max(FourChan(3,:))])
+  cosmo(gca,titl2{3},xlimit2,xtix2,[])
+
+  subplot(4,3,11);
+  plot(FourChan(4,:),'color',color4)
+  hold on
+  plot(mavgFourChan,'LineWidth',1.75)
+  xlabel('Time [s]')
+  ylim([min(FourChan(4,:))-abs(.001*min(FourChan(4,:))) ...
+	max(FourChan(4,:))+(.001*max(FourChan(4,:)))])
+  yticks([min(FourChan(4,:)) round(avg(4)) max(FourChan(4,:))])
+  cosmo(gca,titl2{4},xlimit2,xtix2,xtixl2)
+  
+  % Plot sub
+  xtixl3 = tensec/Fs;
+  xtix3 = (bot-lowbound):Fs:((10*Fs)-1-(upbound-top));
+  xlimit3 = [0 rseg*Fs];
+  titl3 = {rseg + " Second Segment"};
+  
+  subplot(4,3,3)
+  plot(sub(1,:),'color',color1)
+  ylim([4900 5700])
+  yticks([5000 5600])
+  cosmo(gca,titl3{1},xlimit3,xtix3,[])
+
+  subplot(4,3,6)
+  plot(sub(2,:),'color',color2)
+  cosmo(gca,[],xlimit3,xtix3,[])
+
+  subplot(4,3,9);
+  plot(sub(3,:),'color',color3) 
+  cosmo(gca,[],xlimit3,xtix3,[])
+
+  subplot(4,3,12);
+  plot(sub(4,:),'color',color4)
+  hold on
+  plot(mavgsub,'LineWidth',1.75)
+  xlabel('Time [s]')
+  cosmo(gca,[],xlimit3,xtix3,xtixl3)
+      
   sgtitle(['BIOSUnit2, Minute ',num2str(file)])
+
   if exist('jumps','var') ~= 0
     a = annotation('textbox',[0.77 0.94 0 0],'String',['# of jumps = ' num2str(jumps)],'FitBoxToText','on');
     a.FontSize = 12;
@@ -154,7 +203,7 @@ for file = firstfile:lastfile
   
   % Write the PDF image file
   saveas(gcf,sprintf('file%3.4i.pdf',file));
-
+  
   if firstfile == lastfile || file == lastfile
     %if working with 1 file, or working on the last file of a set, don't clf
   else
