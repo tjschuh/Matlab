05ddbede8f97dbce4f47068026c460a6a0915c9e
Frederik J. Simons <fjsimons@gmail.com>
2020-12-18 11:23:08 -0500

    Turns ship trajectory into slant range

---
+++ ship2slant.m
@@ -0,0 +1,36 @@
+function varargout=ship2slant(x,y,t,x0,y0,z0,c,z)
+% [sr,st]=SHIP2SLANT(x,y,t,x0,y0,z0,c,z)
+%
+% INPUT:  
+%
+% x,y,t     Ship trajectory
+% x0,y0     Beacon drop-off point 
+% z0        Nominal ocean depth [m]
+% c,z       Sound speed profile [m/s,m]
+%
+% OUTPUT:
+%
+% sr        Slant range
+% st        Slant time
+%
+% Last modified by tschuh-at-princeton.edu on Dec 18th, 2020
+
+% Default sound speed profile
+defval('c',1500)
+defval('z',NaN)
+
+if nargout > 1
+  % Compute the time
+  if length(c)==1 ||  isnan(z)
+    st=sr/c;
+  else
+    % Do a calculation that converts distance to time
+    % Appoximately? Proper ray tracing?
+  end
+end
+
+
+% Optional outputs
+varns={sr,st,t};
+vararagout=varns(1:nargout);
+
