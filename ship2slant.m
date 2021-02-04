395124864ba2ba451e6a145691c8202e854bbd71
Terance Schuh <tschuh@princeton.edu>
2021-02-03 22:41:36 -0500

    Turns ship trajectory into slant range

--- ship2slant.m
+++ ship2slant.m
@@ -1,11 +1,9 @@
-function varargout=ship2slant(x,y,t,x0,y0,z0,c,z)
-% [sr,st]=SHIP2SLANT(x,y,t,x0,y0,z0,c,z)
+function shiplocations = ship2slant(path,c,z)
+% []=SHIP2SLANT()
 %
 % INPUT:  
 %
-% x,y,t     Ship trajectory
-% x0,y0     Beacon drop-off point 
-% z0        Nominal ocean depth [m]
+% path      Ship trajectory chosen by user, current options: spiral
 % c,z       Sound speed profile [m/s,m]
 %
 % OUTPUT:
@@ -14,7 +12,17 @@ function varargout=ship2slant(x,y,t,x0,y0,z0,c,z)
 % st        Slant time
 %
 % Originally written by tschuh@princeton.edu, 12/18/2020
-% Last modified by tschuh@princeton.edu, 12/18/2020
+% Last modified by tschuh@princeton.edu, 2/3/2021
+  
+% Default ship trajectory is spiral
+defval('path','spiral')
+
+% if path = spiral, call spiralpath to generate shiplocations and drop-off point
+if strcmp(path,'spiral') == 1
+  shiplocations = spiralpath();
+  % beacon drop-off point (x0,y0,z0)
+  drop = [shiplocations(1,1) shiplocations(1,2) shiplocations(1,3)];
+end
 
 % Default sound speed profile
 defval('c',1500)
@@ -32,6 +40,7 @@ end
 
 
 % Optional outputs
-varns={sr,st,t};
-vararagout=varns(1:nargout);
+%varns={sr,st,t};
+%vararagout=varns(1:nargout);
 
+%keyboard
