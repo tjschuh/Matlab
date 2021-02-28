44cb5373f3414698d39f974db3c5f922bc37757c
Terance Schuh <tschuh@princeton.edu>
2021-02-28 11:51:37 -0500

    A rectangular ship trajectory

--- rectanglepath.m
+++ rectanglepath.m
@@ -8,7 +8,7 @@ function [] = rectanglepath(xymin,xymax,nturns)
 % TESTED ON: 9.8.0.1417392 (R2020a) Update 4
 %
 % Originally written by tschuh@princeton.edu, 2/25/2021
-% Last modified by tschuh@princeton.edu, 2/25/2021
+% Last modified by tschuh@princeton.edu, 2/26/2021
 
 % This function creates an RNG rectangular path of a ship on the
 % ocean and a geodetic beacon falling to the seafloor
@@ -20,10 +20,10 @@ defval('xymin',-100);
 defval('xymax',100);
 
 % number of turns
-defval('nturns',3);
+defval('nturns',2);
 
 % length of turns
-turns = 3;
+length = 3;
 
 % distance between edges of rectangle
 range = 8;
@@ -33,24 +33,30 @@ step = 5;
 ship(1,1) = xymin+(xymax-xymin)*rand;
 ship(1,2) = xymin+(xymax-xymin)*rand;
 
-for r = 1:range-1
-  ship(r+1,1) = ship(r,1);
-  ship(r+1,2) = ship(r,2)+step;
+for i = 1:nturns
+  if i == 1
+    for a = 2:range+length-1
+      if a <= range
+	ship(a,1) = ship(a-1,1);
+	ship(a,2) = ship(a-1,2)+step;
+      elseif a > range
+	ship(a,1) = ship(a-1,1)+step;
+	ship(a,2) = ship(a-1,2);
+      end
+    end
+  elseif i > 1
+    for b = (range+length)*(i-1):((range+length)*i)-3
+      if b < range+((range+length-1)*(i-1))
+	ship(b,1) = ship(b-1,1);
+	ship(b,2) = ship(b-1,2)+(((-1)^(i-1))*step);
+      elseif b >= range+((range+length-1)*(i-1))
+	ship(b,1) = ship(b-1,1)+step;
+	ship(b,2) = ship(b-1,2);
+      end
+    end
+  end  
 end
 
-for t = range:range+turns-2
-  ship(t+1,1) = ship(t,1)+step;
-  ship(t+1,2) = ship(t,2);
-end  
 %keyboard
-%for i=1:1%nturns
- % t1 = [start(1,1) start(1,2)+range];
- % t2 = [t1(1)+(height/4) t1(2)];
-%end
-
-
 
 scatter(ship(:,1),ship(:,2),'^','filled')
-%hold on
-%scatter(t1(1),t1(2),'^','filled')
-%scatter(t2(1),t2(2),'^','filled')
