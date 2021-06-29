1a23c817184c3eca70b9a766e4d979934930316f
Terance Schuh <tschuh@princeton.edu>
2021-06-29 15:40:51 -0400

    plot velocity profile from RINEX file approx positions

--- velopro.m
+++ velopro.m
@@ -12,21 +12,29 @@ function velohr=velopro()
 % TESTED ON: 9.8.0.1417392 (R2020a) Update 4
 %
 % Originally written by tschuh-at-princeton.edu, 06/23/2021
-% Last modified by tschuh-at-princeton.edu, 06/23/2021
+% Last modified by tschuh-at-princeton.edu, 06/29/2021
 
-load approx_positions
-pos=approx_positions;
+load obsfiles.pos
+pos=obsfiles(:,1:3);
+t1=datetime(obsfiles(:,4:9));
+t2=datetime(obsfiles(:,10:15));
 
-velosc=zeros(length(pos)-1,1);
-velohr=zeros(length(pos)-1,1);
+dist=zeros(length(pos)-1,1);
+velo=zeros(length(pos)-1,1);
 for i=2:length(pos)
-    velohr(i-1,1)=sqrt((pos(i,1)-pos(i-1,1))^2 + (pos(i,2)-pos(i-1,2))^2 + (pos(i,3)-pos(i-1,3))^2);
-    velosc(i-1,1)=velohr(i-1,1)/3600;
+    dist(i-1,1)=sqrt((pos(i,1)-pos(i-1,1))^2 + (pos(i,2)-pos(i-1,2))^2 + (pos(i,3)-pos(i-1,3))^2);
+    velo(i-1,1)=dist(i-1,1)/60
 end
-plot(velohr,'LineWidth',1.5)
+
+% 1 knot = 1852 m/hr
+
+f=figure;
+%plot(velo,'LineWidth',1.5)
+plot(t1(2:end),velo,'-o','LineWidth',1.5)
 grid on
-xlim([1 length(pos)-1])
-xlabel('Time [hr]')
+axis tight
+%xlim([1 length(pos)-1])
+xlabel('Time')
 ylabel('Velocity [m/hr]')
-%figure
-%plot(velosc)
+set(f,'renderer','painters')
+keyboard
