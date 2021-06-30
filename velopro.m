bd14af1147829897897dca9d90b27154c5b107ad
Terance Schuh <tschuh@princeton.edu>
2021-06-30 15:15:36 -0400

    Plot velocity profile from RINEX file approx positions

--- velopro.m
+++ velopro.m
@@ -1,5 +1,10 @@
-function velohr=velopro()
-%velo=VELOPRO()
+function velopro()
+%VELOPRO()
+%
+% Takes in data file containing XYZ positions and timestamps
+% Produces a velocity profile from the position data by
+% computing the distance between each position and dividing
+% the result by the time interval between each position
 %
 % INPUT:
 %
@@ -9,16 +14,21 @@ function velohr=velopro()
 %
 %
 %
-% TESTED ON: 9.8.0.1417392 (R2020a) Update 4
+% TESTED ON: 9.4.0.813654 (R2018a)
 %
 % Originally written by tschuh-at-princeton.edu, 06/23/2021
 % Last modified by tschuh-at-princeton.edu, 06/29/2021
 
+% load in data file
+% column order: X Y Z  YYYY MM DD HH MM SS  YYYY MM DD HH MM SS
 load obsfiles.pos
 pos=obsfiles(:,1:3);
 t1=datetime(obsfiles(:,4:9));
 t2=datetime(obsfiles(:,10:15));
 
+% compute euclidean distance between each xyz position
+% divide by appropriate time interval for desired units
+% default: knots 
 dist=zeros(length(pos)-1,1);
 velo=zeros(length(pos)-1,1);
 for i=2:length(pos)
@@ -33,13 +43,37 @@ velo=dist;
 % 1 knot = 1852 m/hr
 velo=velo./1852;
 
+% plotting
+e=figure;
+plot(t1(2:end),velo,'LineWidth',1.5)
+cosmo
+ylim([0 ceil(max(velo))])
+
 f=figure;
-%plot(velo,'LineWidth',1.5)
-plot(t1(2:end),velo,'-o','LineWidth',1.5)
-grid on
-axis tight
-%xlim([1 length(pos)-1])
+plot(t1(2:84),velo(1:83),'LineWidth',1.5)
+cosmo
+ylim([0 ceil(max(velo(1:83)))])
+
+g=figure;
+plot(t1(85:110),velo(84:109),'LineWidth',1.5)
+cosmo
+ylim([0 ceil(max(velo(84:109)))])
+
+h=figure;
+plot(t1(110:end),velo(109:end),'LineWidth',1.5)
+cosmo
+ylim([0 ceil(max(velo(109:end)))])
+
+set(e,'renderer','painters')
+set(f,'renderer','painters')
+set(g,'renderer','painters')
+set(h,'renderer','painters')
+
+function cosmo
 xlabel('Time')
 ylabel('Velocity [knots]')
 title('Velocity Profile of RV Atlantic Explorer ')
-set(f,'renderer','painters')
+grid on
+axis tight
+longticks
+
