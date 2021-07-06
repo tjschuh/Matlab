02c9577279e7fa16e8731b603daefe227ad510a8
Terance Schuh <tschuh@princeton.edu>
2021-07-06 16:19:10 -0400

    Plot 2D lat lon position plot of BIOS cruise data

---
+++ llhplot.m
@@ -0,0 +1,27 @@
+function llhplot()
+%LLHPLOT()
+%
+% Takes in data file containing llh positions
+% Produces a 2D lat vs lon position plot
+%
+% INPUT:
+%
+% OUTPUT:
+%
+% TESTED ON: 9.4.0.813654 (R2018a)
+%
+% Originally written by tschuh-at-princeton.edu, 07/06/2021
+
+% load in data file
+load obsfiles.llh
+
+f=figure;
+plot(obsfiles(:,2),obsfiles(:,1))
+hold on
+scatter(obsfiles(:,2),obsfiles(:,1),'filled')
+grid on
+title('June 16 Lat/Lon Ship Coordinates')
+xlabel('Longitude [degrees]')
+ylabel('Latitude [degrees]')
+
+set(f,'renderer','painters')
