4cda01d093f4ae0b2693ef32e8c2bc4540e0567e
Terance Schuh <tschuh@princeton.edu>
2021-08-20 17:38:21 -0400

    Inserts NaNs where distances between consecutive points are too big

--- xyzpenlift.m
+++ xyzpenlift.m
@@ -1,5 +1,5 @@
-function varargout = xyzpenlift(file)
-% XYZ=XYZPENLIFT(file)
+function varargout = xyzpenlift(file,xcol,dlev)
+% XYZ=XYZPENLIFT(file,xcol,dlev)
 %
 % Computes Euclidean distance between
 % consecutive XYZ points and if the
@@ -9,53 +9,42 @@ function varargout = xyzpenlift(file)
 % INPUT:
 %
 % file   data file containing XYZ coordinates
-%
+% xcol   column number in file where XYZ-LLH data begins [default: 19]
+% dlev   threshold distance, if exceeded replaced with a NaN [default: 3]
+%   
 % OUTPUT:
 %
 % XYZ    3 column data file that has same dimensions as input file, but with added NaNs
 %
 % Originally written by tschuh-at-princeton.edu, 08/20/2021
 
-% To do:    
-% need to define dlev as an input
-% need to change how data is input b/c right now
-% code works on XYZ coordinates, but also need to
-% change respective LLH coordinates 
+% To do:
+% need to change output so it overwrites original input file
 
 % dlev is the threshold distance
 % dlev = ship speed [m/s] * data sampling rate [s]
-dlev = 3;
+defval('dlev',3)
 
 % load in data file
 data = load(file);
 
-% make this a defval
-XYZ = data(:,19:21);
-X = XYZ(:,1);
-Y = XYZ(:,2);
-Z = XYZ(:,3);
+% we want XYZ cols and LLH cols
+defval('xcol',19)
+XYZ = data(:,xcol:xcol+5);
 
 % Distance between two consecutive points on the sphere
-d = sqrt([diff(X)].^2 + [diff(Y)].^2 + [diff(Z)].^2);
+d = sqrt([diff(XYZ(:,1))].^2 + [diff(XYZ(:,2))].^2 + [diff(XYZ(:,3))].^2);
 
 % Where do the NaN's go (linear index is enough here)
 p = find(d>dlev*nmn(d));
 
-% using p, replace certain rows in XYZ with NaNs
+% Using p, replace certain rows in XYZ with NaNs
 for i = 1:length(p)
-   X(p(i)+1) = NaN;
-   Y(p(i)+1) = NaN;
-   Z(p(i)+1) = NaN;
+   XYZ(p(i)+1,:) = NaN;
 end
 
-% Output preparation
-XYZ(:,1) = X;
-XYZ(:,2) = Y;
-XYZ(:,3) = Z;
-vars = {XYZ};
-
-% Actual output generation
-varargout = vars;
+% Output generation
+varargout = {XYZ};
 
 end
 
