81e3f4331b74a72e016b3b67e24d056aefcbd4c8
Terance Schuh <tschuh@princeton.edu>
2021-08-31 23:48:01 -0400

    Inserts NaNs where distances between consecutive points are too big

--- xyzpenlift.m
+++ xyzpenlift.m
@@ -1,75 +1,97 @@
-function varargout = xyzpenlift(file,xcol,dlev)
-% XYZPENLIFT(file,xcol,dlev)
+function xyzpenlift(file,xcol,tcol,velo,loop)
+% XYZPENLIFT(file,xcol,tcol,velo,loop)
 %
 % Computes Euclidean distance between
 % consecutive XYZ points and if the
 % jump is too big it replaces the
 % second point with a NaN
+% done in a user-defined for loop
+% to catch the max number of jumps
 %
 % INPUT:
 %
 % file   data file containing XYZ coordinates
 % xcol   column number in file where XYZ-LLH data begins [default: 19]
-% dlev   threshold distance, if exceeded replaced with a NaN [default: 3]
-%   
+% tcol   column number in file where time begin [default: 7]
+% velo   ship speed used to compute threshold distance triggering NaN replacement [default: 3]
+% loop   number of loop iterations to perform [default: 50] 
+%
 % OUTPUT:
 %
-% modified file
+% modified file data.ppp
 %
 % Originally written by tschuh-at-princeton.edu, 08/20/2021
-% Last modified by tschuh-at-princeton.edu, 08/24/2021
-    
+% Last modified by tschuh-at-princeton.edu, 08/31/2021
+
 % should make inputs like Frederik's penlift.m
 % s.t. you can input ascii or mat file
-
-% Problem:
-% 10 consecutive points are clearly too far away
-% currently only the first one gets turned to NaN
-% because we are only comparing a point to the previous one
-% Solution:
-% turn function into a for loop that runs algorithm,
-% then runs it again ignoring all NaN
-    
-% dlev is the threshold distance
-% dlev = ship speed [m/s] * data sampling rate [s]
-defval('dlev',3);
 
 % load in data file
 data = load(file);
 
-% we want XYZ cols and LLH cols
+% we want XYZ cols and LLH cols to turn to NaNs
 defval('xcol',19);
-XYZ = data(:,xcol:xcol+5);
+
+% we also want time columns to keep time
+defval('tcol',7);
+
+% ship speed really is 1.03 m/s
+defval('velo',3);
+
+% number of loop iterations to perform
+defval('loop',50);
+
+% remove entire NaN rows that
+% may exist in data from start
+rows=any(isnan(data),2);
+data(rows,:)=[];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% to remove entire sections of glitches that might be stacked
+% on one another we use a for loop such that the first time thru
+% a glitch may be caught, but others which appear right on top
+% of it only seconds later in the data are not caught
+% to counter this, we turn gltiches to NaNs, remove entire
+% rows containing NaNs, then run the loop again bearing in
+% mind the sampling rate between some data points has increased
+
+for k = 1:loop
+
 % Distance between two consecutive points on the sphere
-d = sqrt([diff(XYZ(:,1))].^2 + [diff(XYZ(:,2))].^2 + [diff(XYZ(:,3))].^2);
+d = sqrt([diff(data(:,xcol))].^2 + [diff(data(:,xcol+1))].^2 + [diff(data(:,xcol+2))].^2);
 
-% Where do the NaN's go (linear index is enough here)
-%p = find(d>dlev*nmn(d));
-p = find(d>dlev);
+% duration in seconds between consecutive points (use seconds function)
+dat = datetime(data(:,tcol:tcol+5));
+t = seconds(diff(dat));
 
-% Using p, replace certain rows in XYZ with NaNs
-for i = 1:length(p)
-   XYZ(p(i)+1,:) = NaN;
+% for every d value check to see if > dlev (threshold distance)
+% if so, that point in the data is a glitch so turn it to NaN
+% dlev is computed based on the ship velocity AND how much time
+% is in-between data points i.e. if 3 seconds have gone by, then
+% the ship could have moved a max 3*velo meters
+for j = 1:length(d)
+    % dlev is the threshold distance
+    % dlev = ship speed [m/s] * data sampling rate [s]
+    % dlev = shipspeed*diff between consecutive times
+    dlev = velo*t(j);
+    if dlev < 1
+        dlev = velo;
+    end    
+    if d(j) > dlev
+        data(j+1,xcol:xcol+5) = NaN;
+    end
+end
+
+% remove entire NaN rows that
+% were just created during loop
+rows=any(isnan(data),2);
+data(rows,:)=[];
+
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-data(:,xcol:xcol+5) = XYZ;
-
-% Output generation
+% output generation
+% save final data file to data.ppp
 save('data.ppp','data','-ascii')
-%varargout = {data};
-end
-
-function y = nmn(x)
-    % count how many entries of your vector are nans 
-    n = sum(~isnan(x));
-    % replace the nans in your vector with zeroes 
-    % such that they don't contribute to the sum    
-    x(isnan(x)) = 0;
-    % Divide the sum by number of non-nan elements
-    y = sum(x)/n;
-end
