b8fec1474ce046c09af442266ad4759b1c1920d2
Terance Schuh <tschuh@princeton.edu>
2021-09-29 17:58:58 -0400

    Remove rows containing NaNs from a matrix

---
+++ rmNaNrows.m
@@ -0,0 +1,21 @@
+function rmNaNrows(data)
+% RMNANROWS(data)
+%
+% given a matrix, if a row has
+% a NaN in even a single column
+% remove the entire row    
+%
+% INPUT:
+%
+% data            standard NxM data matrix
+%
+% OUTPUT:
+%
+% modified data   same input data matrix, but with no NaN rows
+%
+% TESTED ON: 9.4.0.813654 (R2018a)
+%
+% Originally written by tschuh-at-princeton.edu, 09/03/2021
+
+rows=any(isnan(data),2);
+data(rows,:)=[];    
